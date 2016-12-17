7819a45822c35434846a18a43975231cd8adb832
Gabriel Casarin <gabriel.casarin@hotmail.com>
2016-12-17 18:11:14 -0200

    adicionado modo offline em gui.py

--- gui.py
+++ gui.py
@@ -3,6 +3,8 @@ import socket
 import pygame
 pygame.init()
 
+MODO_OFFLINE = False
+
 with open('config/config.json') as jfile:
     config = json.load(jfile)
     cores = config['cores']
@@ -24,7 +26,7 @@ bgrect = bg.get_rect()
 SCREEN.blit(bg,bgrect)
 pygame.display.update()
 
-print(IMAGES_BUTTONS)
+#print(IMAGES_BUTTONS)
 
 class Samurai(pygame.sprite.Sprite):
     def __init__(self, img_name):
@@ -72,7 +74,6 @@ class Cliente:
         self.board = Board(15)
 
         self.screen = SCREEN
-        #self.screen.fill((255,255,255))
 
         cmx = 530#centerMoveX
         cmy = 80#centerMoveY
@@ -103,20 +104,6 @@ class Cliente:
         }
 
 
-        # pygame.draw.rect(self.screen,(20,20,20),self.Acoes['move_down']['rect'])
-        # pygame.draw.rect(self.screen,(20,20,20),self.Acoes['move_left']['rect'])
-        # pygame.draw.rect(self.screen,(20,20,20),self.Acoes['move_up']['rect'])
-        # pygame.draw.rect(self.screen,(20,20,20),self.Acoes['move_right']['rect'])
-
-        # pygame.draw.rect(self.screen,(20,20,20),self.Acoes['occupy_down']['rect'])
-        # pygame.draw.rect(self.screen,(20,20,20),self.Acoes['occupy_left']['rect'])
-        # pygame.draw.rect(self.screen,(20,20,20),self.Acoes['occupy_up']['rect'])
-        # pygame.draw.rect(self.screen,(20,20,20),self.Acoes['occupy_right']['rect'])
-
-        # pygame.draw.rect(self.screen,(20,20,20),self.Acoes['Hide']['rect'])
-
-        # pygame.draw.rect(self.screen,(20,20,20),self.Acoes['Send']['rect'])
-        # pygame.draw.rect(self.screen,(20,20,20),self.Acoes['Erase']['rect'])
         self.screen.blit(IMAGES_BUTTONS['Down'],self.Acoes['move_down']['rect'])
         self.screen.blit(IMAGES_BUTTONS['Right'],self.Acoes['move_right']['rect'])
         self.screen.blit(IMAGES_BUTTONS['Up'],self.Acoes['move_up']['rect'])
@@ -134,14 +121,16 @@ class Cliente:
 
 
 
+        if not MODO_OFFLINE:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            with open('config/config.json') as jfile:
+                config = json.load(jfile)
+                self.sock.connect((config["ip"], config["port"]))
+                self.num = int(str(self.sock.recv(1), 'ascii'))
+        else:
+            self.num = 1
 
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        with open('config/config.json') as jfile:
-            config = json.load(jfile)
-            self.sock.connect((config["ip"], config["port"]))
-            self.num = int(str(self.sock.recv(1), 'ascii'))
-            print('sou o player', self.num)
-
+        print('sou o player', self.num)
 
 
         self.samurais = {
@@ -155,58 +144,104 @@ class Cliente:
 
 
     def run(self):
-        
-        while True:
-            self.estado = 'inicial'
-            turno = self.request_turn()
-            self.turno_atual = int(turno[0])
+        if not MODO_OFFLINE:
+            while True:
+                self.estado = 'inicial'
+                turno = self.request_turn()
+                self.turno_atual = int(turno[0])
 
-            self.order = []
+                self.order = []
 
-            tabuleiro = turno[7:]
-            for i in range(len(tabuleiro)):
-                tabuleiro[i] = tabuleiro[i].split()
-                for j in range(len(tabuleiro[i])):
-                    self.board.casas[(i,j)]['cor'] = cores[tabuleiro[i][j]]
-            self.board.draw(self.screen)
+                tabuleiro = turno[7:]
+                for i in range(len(tabuleiro)):
+                    tabuleiro[i] = tabuleiro[i].split()
+                    for j in range(len(tabuleiro[i])):
+                        self.board.casas[(i,j)]['cor'] = cores[tabuleiro[i][j]]
+                self.board.draw(self.screen)
 
-            for num_samurai, samurai in self.samurais.items():
-                x, y, order_status, hidden, treatment = turno[num_samurai].split()
-                x, y = int(x), int(y)
-                samurai.hidden = int(hidden)
-                if (x,y) != (-1, -1):
-                    samurai.set_center(self.board.casas[(x,y)]['rect'].center)
-                    samurai.draw(self.screen)
-                samurai.treatment = int(treatment)
-                samurai.order_status = int(order_status)
+                for num_samurai, samurai in self.samurais.items():
+                    x, y, order_status, hidden, treatment = turno[num_samurai].split()
+                    x, y = int(x), int(y)
+                    samurai.hidden = int(hidden)
+                    if (x,y) != (-1, -1):
+                        samurai.set_center(self.board.casas[(x,y)]['rect'].center)
+                        samurai.draw(self.screen)
+                    samurai.treatment = int(treatment)
+                    samurai.order_status = int(order_status)
 
-            pygame.display.update()
+                pygame.display.update()
 
-            print(self.turno_atual%2+1,self.num)
-            if self.turno_atual%2+1 == self.num:
-                while self.estado != 'enviar_comandos':
-                    for event in pygame.event.get():
-                        if event.type == pygame.MOUSEBUTTONDOWN:
-                            for nome, botao in self.Acoes.items():
-                                if botao['rect'].collidepoint(event.pos):
-                                    if botao['num'] == -1:
-                                        if self.order:
-                                            self.order.pop()
-                                    else:
-                                        self.order.append(str(botao['num']))
-                                        if botao['num'] == 0:
-                                            self.estado = 'enviar_comandos'
-                                    print(self.order)
-                        elif event.type == pygame.QUIT:
-                            pygame.quit()
-                            self.sock.close()
+                print(self.turno_atual%2+1,self.num)
+                if self.turno_atual%2+1 == self.num:
+                    while self.estado != 'enviar_comandos':
+                        for event in pygame.event.get():
+                            if event.type == pygame.MOUSEBUTTONDOWN:
+                                for nome, botao in self.Acoes.items():
+                                    if botao['rect'].collidepoint(event.pos):
+                                        if botao['num'] == -1:
+                                            if self.order:
+                                                self.order.pop()
+                                        else:
+                                            self.order.append(str(botao['num']))
+                                            if botao['num'] == 0:
+                                                self.estado = 'enviar_comandos'
+                                        print(self.order)
+                            elif event.type == pygame.QUIT:
+                                pygame.quit()
+                                self.sock.close()
 
-                #envia os comandos para o server aqui
-                self.sock.sendall(bytes(' '.join(self.order), 'ascii'))
+                    #envia os comandos para o server aqui
+                    self.sock.sendall(bytes(' '.join(self.order), 'ascii'))
+        else:  # se MODO_OFFLINE
+                self.estado = 'inicial'
+                turno = self.request_turn()
+                self.turno_atual = int(turno[0])
+
+                self.order = []
+
+                tabuleiro = turno[7:]
+                for i in range(len(tabuleiro)):
+                    tabuleiro[i] = tabuleiro[i].split()
+                    for j in range(len(tabuleiro[i])):
+                        self.board.casas[(i,j)]['cor'] = cores[tabuleiro[i][j]]
+                self.board.draw(self.screen)
+
+                for num_samurai, samurai in self.samurais.items():
+                    x, y, order_status, hidden, treatment = turno[num_samurai].split()
+                    x, y = int(x), int(y)
+                    samurai.hidden = int(hidden)
+                    if (x,y) != (-1, -1):
+                        samurai.set_center(self.board.casas[(x,y)]['rect'].center)
+                        samurai.draw(self.screen)
+                    samurai.treatment = int(treatment)
+                    samurai.order_status = int(order_status)
+
+                pygame.display.update()
+
+                print(self.turno_atual%2+1,self.num)
+                if self.turno_atual%2+1 == self.num:
+                    while self.estado != 'enviar_comandos':
+                        for event in pygame.event.get():
+                            if event.type == pygame.MOUSEBUTTONDOWN:
+                                for nome, botao in self.Acoes.items():
+                                    if botao['rect'].collidepoint(event.pos):
+                                        if botao['num'] == -1:
+                                            if self.order:
+                                                self.order.pop()
+                                        else:
+                                            self.order.append(str(botao['num']))
+                                            if botao['num'] == 0:
+                                                self.estado = 'enviar_comandos'
+                                        print(self.order)
+                            elif event.type == pygame.QUIT:
+                                pygame.quit()
 
     def request_turn(self):
         print('Aguardando envio de informações de turno por parte do Game Manager...')
-        turno = str(self.sock.recv(1024), "ascii").split('\n')
+        if not MODO_OFFLINE:
+            turno = str(self.sock.recv(1024), "ascii").split('\n')
+        else:
+            turno = open('info.txt').read().split('\n')
         print('Dados recebidos')
         print(turno)
         return turno
