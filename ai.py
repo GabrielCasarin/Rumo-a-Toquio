0169f99725384271ddbd891efecbc64c5ed4f8b0
Gabriel Casarin <gabriel.casarin@hotmail.com>
2017-01-12 18:11:35 -0200

    criada RN

--- ai.py
+++ ai.py
@@ -7,6 +7,24 @@
 ##################################################################
 
 
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Input, Dense, Activation
+
+
+Q = Sequential()
+Q.add(Dense(output_dim=40, input_dim=80))
+Q.add(Activation("sigmoid"))
+Q.add(Dense(output_dim=40))
+Q.add(Activation("sigmoid"))
+Q.add(Dense(output_dim=30))
+Q.compile(loss='mean_squared_error', optimizer='SGD')
+
+epsilon = 0.2
+batch_size = 16
+gamma = 0.9
+
+
 def search(game):
 	pass
 
