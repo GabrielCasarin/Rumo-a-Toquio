2786ef1a4e8fe2fec746c9b1098dd7cb1409d1fa
Gabriel Casarin <gabriel.casarin@hotmail.com>
2017-01-16 20:18:11 -0200

    bd para estados

--- ai.py
+++ ai.py
@@ -7,17 +7,24 @@
 ##################################################################
 
 
+import hashlib
+
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Input, Dense, Activation
+
+from BTrees.OOBTree import OOBTree
+import ZODB
+import ZODB.FileStorage as FS
+import transaction
+import persistent
 
 from config import *
 
 
 class AI:
-	def __init__(self, G, player, treinar, camadas=[82, 40, 30]):
+	def __init__(self, player, treinar, camadas=[82, 40, 30]):
 		super(AI, self).__init__()
-		self.G = G
 		self.player = player
 		self.treinar = treinar
 
@@ -32,7 +39,7 @@ class AI:
 
 		self.Q.compile(loss='mean_squared_error', optimizer='SGD')
 
-		self.Q.save('blablabla.h5')
+		self.Q.save('model{}.h5'.format(self.player))
 
 		self.epsilon = 0.2
 		self.batch_size = 16
@@ -52,10 +59,6 @@ class AI:
 		for i in range(len(tabuleiro)):
 			for j in range(len(tabuleiro)):
 				tabuleiro[i][j] = int(tabuleiro[i][j])
-
-		# print('turno:', turno)
-		# print('samurais:', samurais)
-		# print('tabuleiro:', tabuleiro)
 
 		budget = MAX_BUDGET
 
@@ -92,7 +95,7 @@ class AI:
 		#estado = self.estado.copy()  #TODO IMPORTANTE
 
 		while budget > 0 and acao != 0:
-			vectQ = self.Q.predict(estado.toVect())[0]
+			vectQ = self.Q.predict(estado.to_vect())[0]
 
 			if not listaAcao:
 				#V = np.max(vectQ)
@@ -145,22 +148,66 @@ def select(node):
 def update(node, action, payoff):
 	pass
 
-class Estado:
+
+class Estado(persistent.Persistent):
+
 	def __init__(self, turno, samurais, tabuleiro, budget, player):
 		self.turno = turno			#int 0 95
 		self.samurais = samurais	#int de lista de lista (6x5)
-		self.tabuleiro = tabuleiro  #int de lista de lista (sizexsize)
+		self.tabuleiro = tabuleiro  #int de lista de lista (size x size)
 		self.budget = budget		#int 0 7
 		self.player = player		#int 0 1
+		self.qtd_visitas = 0        # para futuras implementacoes
 
-	def toVect(self):
-		tam =  33 + len(self.tabuleiro)**2
+	# GETTERS E SETTERS
+
+	# Samurais
+	@property
+	def samurais(self):
+		return self.__samurais
+
+	@samurais.setter
+	def samurais(self, s):
+		self.__samurais = []
+		for el in s:
+			self.__samurais.append(list(el))
+		self._p_changed = True
+
+	# Tabuleiro
+	@property
+	def tabuleiro(self):
+		return self.__tabuleiro
+
+	@tabuleiro.setter
+	def tabuleiro(self, s):
+		self.__tabuleiro = []
+		for el in s:
+			self.__tabuleiro.append(list(el))
+		self._p_changed = True
+
+	# FIM GETTERS E SETTERS
+
+	def to_hash(self):
+		string = str(self.turno)
+		for i in range (len(self.samurais)):
+			for j in range (5):
+				string += str(self.samurais[i][j])
+		for i in range (len(self.tabuleiro)):
+			for j in range (len(self.tabuleiro)):
+				string += str(self.tabuleiro[i][j])
+		string += str(self.budget)
+		string += str(self.player)
+
+		return hashlib.sha256(string.encode()).hexdigest()
+
+	def to_vect(self):
+		tam = 33 + len(self.tabuleiro)**2
 
 		vect = np.ndarray((1,tam))
 		k = 0
 
-		vect[0][0] = self.turno
-		k+= 1
+		vect[0][k] = self.turno
+		k += 1
 
 		for i in range (len(self.samurais)):
 			for j in range (5):
@@ -169,7 +216,6 @@ class Estado:
 
 		for i in range (len(self.tabuleiro)):
 			for j in range (len(self.tabuleiro)):
-
 				vect[0][k] = self.tabuleiro[i][j]
 				k += 1
 
@@ -181,5 +227,49 @@ class Estado:
 		return vect
 
 	#TODO
-	def copy(self, arg):
-		pass
+	def copy(self):
+		novo_estado = Estado(
+			turno=self.turno,
+			samurais=self.samurais,
+			tabuleiro=self.tabuleiro,
+			budget=self.budget,
+			player=self.player
+		)
+		return novo_estado
+
+class EstadosDB:
+	def __init__(self, arq='estados.fs'):
+		self.storage = ZODB.FileStorage.FileStorage(arq)  # armazena os dados fisicamente no arquivo .fs
+		self.db = ZODB.DB(storage)  # encapsula o objeto de armazenamento (storage), além de prover o comportamento do DB
+		self.conn = db.open()  # começa uma conexão com o DB a fim de podermos realizar transações
+		self.dbroot = conn.root()  # o objeto root funciona como um namespace para todos os outros contêineres do DB
+		if 'estados' not in self.dbroot.keys():
+			self.dbroot['estados'] = OOBTree()
+		self.estados = self.dbroot['estados']
+
+	def get_estado(self, turno, samurais, tabuleiro, budget, player):
+		# calcula o hash
+		string = str(turno)
+		for i in range (len(samurais)):
+			for j in range (5):
+				string += str(samurais[i][j])
+		for i in range (len(tabuleiro)):
+			for j in range (len(tabuleiro)):
+				string += str(tabuleiro[i][j])
+		string += str(budget)
+		string += str(player)
+
+		h = hashlib.sha256(string.encode()).hexdigest()
+
+		if h in self.estados.keys():
+			return self.estados[h]
+		else:
+			novo_estado = Estado(turno, samurais, tabuleiro, budget, player)
+			self.estados[h] = novo_estado
+			transaction.commit()
+			return novo_estado
+
+	def close():
+		self.conn.close()
+		self.db.close()
+		self.storage.close()
