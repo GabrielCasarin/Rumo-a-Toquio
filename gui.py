f60242fa67cdd80336f974810176cc52b578d785
Victor Roncalli <victor.roncalli.souza@gmail.com>
2017-01-21 19:38:50 -0200

    Branches Unificadas

--- gui.py
+++ gui.py
@@ -6,8 +6,6 @@
 ##                                                              ##
 ##################################################################
 
-#teste
-
 import json
 import socket
 import select
