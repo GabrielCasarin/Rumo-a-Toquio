27de57fa2b78366ce0bca5f391ad1971d8cef3a9
Gabriel Casarin <gabriel.casarin@hotmail.com>
2017-03-14 01:50:32 -0300

    add campos em confi

--- SamurAI/config/__init__.py
+++ SamurAI/config/__init__.py
@@ -1,3 +1,5 @@
+import os.path
+
 MAX_BUDGET = 4
 MAX_TURN = 12
 DIST_VISAO = 5
@@ -5,3 +7,5 @@ TOTALMENTE_OBSERVAVEL = True
 SIZE = 7
 MODO_OFFLINE = False
 SPLASH_SCREEN = False
+ROOT_DIR = os.path.join('.', 'SamurAI')
+DIR = os.path.join(ROOT_DIR, 'database', 'IAs')
