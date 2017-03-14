ca57498a36a82bf21a8fe9fb80606003461eea15
Gabriel Casarin <gabriel.casarin@hotmail.com>
2017-03-14 01:51:51 -0300

    gui_ia refatorada

--- SamurAI/interface/gui_ia.py
+++ SamurAI/interface/gui_ia.py
@@ -5,51 +5,18 @@
 ##      Todos podem usar este codigo livremente                 ##
 ##                                                              ##
 ##################################################################
-
+import os.path
 import json
-import select
 import pygame
-pygame.init()
+import SamurAI.config import as conf
 
 
-from ..config import *
-
-PRINT_DADOS = True
-
-with open('./SamurAI/config/config.json') as jfile:
+with open(os.path.join(conf.ROOT_DIR, 'config', 'config.json') as jfile:
     config = json.load(jfile)
     cores = config['cores']
     IMG_NAMES = config['IMG_SamurAI-Images']
     IMG_NAMES_BUTTONS = config['IMG_buttons-Images']
     IMG_NAMES_INFO = config['IMG_info-Images']
-
-pygame.display.set_icon(pygame.image.load("./SamurAI/images/SamurAI-Images/Icon.png"))
-pygame.display.set_caption('Samurai3x3 AI Battle')
-
-SCREEN      = pygame.display.set_mode((800,600))
-
-SCREEN.fill([220,220,220])
-
-IMAGES      = {
-                name: pygame.image.load("./SamurAI/images/SamurAI-Images/{}.png".format(name)).convert_alpha()
-                for name in IMG_NAMES
-                }
-
-IMAGES_BUTTONS = {
-                name: pygame.image.load("./SamurAI/images/buttons-Images/{}.png".format(name)).convert_alpha()
-                for name in IMG_NAMES_BUTTONS
-                }
-
-IMAGES_INFO = {
-                name: pygame.image.load("./SamurAI/images/info-Images/{}.png".format(name)).convert_alpha()
-                for name in IMG_NAMES_INFO
-                }
-
-
-myfont = pygame.font.SysFont("arial", 15)
-
-
-pygame.display.update()
 
 
 class Board:
@@ -64,54 +31,55 @@ class Board:
             } for i in range(n) for j in range(n)
         }
 
-    def __getitem__(self, key):
-        return self.casas[key]
-
     def draw(self, surface):
         for casa in self.casas.values():
             pygame.draw.rect(surface, casa["cor"], casa["rect"])
             pygame.draw.rect(surface, (0,0,0), casa["rect"].copy(), 1)
 
-
-    def setBoard(self,tabuleiro_separado):
+    def setBoard(self, tabuleiro_separado):
         for j in range(len(tabuleiro_separado)):
             tabuleiro_separado[j] = tabuleiro_separado[j].split()
             for i in range(len(tabuleiro_separado[j])):
-                self[i, j]['cor'] = cores[tabuleiro_separado[j][i]]
+                self.casas[i, j]['cor'] = cores[tabuleiro_separado[j][i]]
+
 
 class Samurai(pygame.sprite.Sprite):
-    def __init__(self, num):
+    def __init__(self, num, IMAGES, IMAGES_BUTTONS, IMAGES_INFO):
+        super(Samurai, self).__init__()
+    
+        self.IMAGES = IMAGES
+        self.IMAGES_BUTTONS = IMAGES_BUTTONS
+        self.IMAGES_INFO = IMAGES_INFO
 
         self.num = num
 
         if num == 0:
             self.img_name = "Blue-spear"
-            self.image = IMAGES[self.img_name]
+            self.image = self.IMAGES[self.img_name]
 
         elif num == 1:
             self.img_name = "Blue-sword"
-            self.image = IMAGES[self.img_name]
+            self.image = self.IMAGES[self.img_name]
 
         elif num == 2:
             self.img_name = "Blue-battleaxe"
-            self.image = IMAGES[self.img_name]
+            self.image = self.IMAGES[self.img_name]
 
         elif num == 3:
             self.img_name = "Red-spear"
-            self.image = IMAGES[self.img_name]
+            self.image = self.IMAGES[self.img_name]
 
         elif num == 4:
             self.img_name = "Red-sword"
-            self.image = IMAGES[self.img_name]
+            self.image = self.IMAGES[self.img_name]
 
         elif num == 5:
             self.img_name = "Red-battleaxe"
-            self.image = IMAGES[self.img_name]
+            self.image = self.IMAGES[self.img_name]
 
         else:
             print('deu ruim')
 
-        super(Samurai, self).__init__()
         self.rect = self.image.get_rect(center=(-100,-100))
 
         self.x = -1
@@ -123,7 +91,7 @@ class Samurai(pygame.sprite.Sprite):
     def set_center(self, center):
         self.rect = self.image.get_rect(center=center)
 
-    def update(self, board, x, y, order_status, hidden, treatment):
+    def update(self, SCREEN, board, x, y, order_status, hidden, treatment):
         self.x, self.y, self.order_status, self.hidden, self.treatment = int(x), int(y), int(order_status), int(hidden), int(treatment)
 
         if (self.x,self.y) != (-1, -1):
@@ -136,10 +104,10 @@ class Samurai(pygame.sprite.Sprite):
         if self.rect.centerx >= 0 and self.rect.centery >= 0:
             surface.blit(self.image, self.rect)
 
-    def drawStatus(self):
+    def drawStatus(self, SCREEN):
 
         #Posicao de referencia
-        centerStat = (520+140*(self.num//3),320+(self.num%3)*45)
+        centerStat = (520 + 140*(self.num//3), 320 + (self.num%3)*45)
         #centerStat = (520,300+(self.num)*45)
 
         #Box
@@ -179,34 +147,59 @@ class Samurai(pygame.sprite.Sprite):
         infoRect = infoImg.get_rect(center=(centerStat[0]-11,centerStat[1]+11))
         SCREEN.blit(infoImg,infoRect)
 
-        #statusHidden:
-
+        #statusHidden
         if self.hidden == 1:
             hImg = IMAGES_INFO['Hidden']
             hRect = hImg.get_rect(center=(centerStat[0]+11,centerStat[1]-11))
             SCREEN.blit(hImg,hRect)
 
-b=Board(7)
-samurais = [Samurai(i) for i in range(6)]
 
-def cliente(turn_info):
+class ClienteGUI:
+    def __init__(self, fps=1):
+        # configura a janela
+        pygame.init()
+        pygame.display.set_icon(pygame.image.load("./SamurAI/images/SamurAI-Images/Icon.png"))
+        pygame.display.set_caption('Samurai3x3 AI Battle')
+        # configura a tela
+        self.screen = pygame.display.set_mode((800,600))
+        self.screen.fill([220,220,220])
 
-    separado = turn_info.split('\n')
+        IMAGES = {name: pygame.image.load(
+            "./SamurAI/images/SamurAI-Images/{}.png".format(name)).convert_alpha() for name in IMG_NAMES}
 
-    turno_separado = separado[0] #pegando apenas a parte do tabuleiro da string status
-    samurais_separados = separado[1:7] #pegando apenas a parte do tabuleiro da string status
-    tabuleiro_separado = separado[7:] #pegando apenas a parte do tabuleiro da string status
+        IMAGES_BUTTONS = {name: pygame.image.load(
+            "./SamurAI/images/buttons-Images/{}.png".format(name)).convert_alpha() for name in IMG_NAMES_BUTTONS}
 
-    b.setBoard(tabuleiro_separado)
-    b.draw(SCREEN)
+        IMAGES_INFO = {name: pygame.image.load(
+            "./SamurAI/images/info-Images/{}.png".format(name)).convert_alpha() for name in IMG_NAMES_INFO}
 
+        self.myfont = pygame.font.SysFont("arial", 15)
 
-    for i in range (6):
-        dados_samurai=(samurais_separados[i]).split(' ')
-        samurais[i].update(b, dados_samurai[0], dados_samurai[1], 
-            dados_samurai[2], dados_samurai[3], dados_samurai[4])
+        # desenha
+        pygame.display.update()
 
+        self.clock = pygame.time.Clock()
+        self.FPS = fps
 
-    pygame.display.update()
-    pygame.time.delay(1500)
-    # input()
+        # cria um tabuleiro e seus samurais
+        self.tab = Board(conf.SIZE)
+        self.samurais = [Samurai(i, IMAGES, IMAGES_BUTTONS, IMAGES_INFO) for i in range(6)]
+
+    def update(self, turn_info):
+        separado = turn_info.split('\n')
+
+        turno_separado = separado[0] #pegando apenas a parte do tabuleiro da string status
+        samurais_separados = separado[1:7] #pegando apenas a parte do tabuleiro da string status
+        tabuleiro_separado = separado[7:] #pegando apenas a parte do tabuleiro da string status
+
+        self.tab.setBoard(tabuleiro_separado)
+        self.tab.draw(self.screen)
+
+        for i in range (6):
+            dados_samurai = samurais_separados[i].split(' ')
+            samurais[i].update(self.screen, self.tab, dados_samurai[0], dados_samurai[1], 
+                dados_samurai[2], dados_samurai[3], dados_samurai[4])
+        pygame.display.update()
+
+    def tick(self):
+        self.clock.tick(self.FPS)
